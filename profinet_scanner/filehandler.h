fd9a7a9e3837a7065c2dc694da886686fd9aa975
Stefan <43118401+Eiwanger@users.noreply.github.com>
2019-07-15 09:10:09 +0200

    Delete filehandler.h

--- profinet_scanner/filehandler.h
+++
@@ -1,8 +0,0 @@
-#include "stdafx.h"
-
-int writeToFile(datasheet* deviceInfo, char* cFilename);
-bool FileExists(const char *fname);
-
-// postition of the curser bevor the last tag
-fpos_t endpos;
-
